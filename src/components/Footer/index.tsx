c337e3cecd1e0dbddfe9c4d7745edca64bb27952
fogge <fogge.bruce@gmail.com>
2024-04-09 18:27:47 +0800

    first commit

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,29 @@
+import { UsergroupDeleteOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <DefaultFooter
+      style={{
+        background: 'none',
+      }}
+      links={[
+        {
+          key: 'Author',
+          title: <UsergroupDeleteOutlined />,
+          href: 'https://t.me/Ro15888',
+          blankTarget: true,
+        },
+        {
+          key: 'telegram',
+          title: 'Ro',
+          href: 'https://t.me/Ro15888',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
+
+export default Footer;
